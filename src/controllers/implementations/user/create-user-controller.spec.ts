2d0fa085e0cab3d841bae7f3a95b1fe5089ef97f
Diandria Xavier <diandriaxavier@gmail.com>
2022-03-16 22:50:57 -0300

    test: create unit test for create user controller

---
+++ src/controllers/implementations/user/create-user-controller.spec.ts
@@ -0,0 +1,56 @@
+import { CreateUserController } from './create-user-controller'
+import { ServerError } from '../../errors'
+import { serverError, success } from '../../helpers/http-helpers'
+import { makeCreateUserUseCase } from '../../../usecases/mock'
+import { CreateUserContract } from '../../../usecases/contracts'
+import { HttpRequest } from '../../contracts'
+
+interface SutTypes {
+  sut: CreateUserController
+  createUserUseCaseStub: CreateUserContract
+}
+
+const makeSut = (): SutTypes => {
+  const createUserUseCaseStub = makeCreateUserUseCase()
+  const sut = new CreateUserController(createUserUseCaseStub)
+  return {
+    sut,
+    createUserUseCaseStub
+  }
+}
+
+const mockBody = {
+  name: 'any_name',
+  email: 'any_email@mail.com',
+  password: 'any_password',
+  phoneNumber: '(00)1234-5678'
+}
+
+const makeRequest = (body: any): HttpRequest => ({ body })
+
+describe('Create User Controller', () => {
+  test('Should return 500 if CreateUserUseCase throws', async () => {
+    const { sut, createUserUseCaseStub } = makeSut()
+    jest.spyOn(createUserUseCaseStub, 'execute').mockImplementationOnce(() => {
+      throw new Error()
+    })
+
+    const httpResponse = await sut.handle(makeRequest(mockBody))
+    expect(httpResponse).toEqual(serverError(new ServerError(null)))
+  })
+
+  test('Should return 200', async () => {
+    const { sut } = makeSut()
+    const httpResponse = await sut.handle(makeRequest(mockBody))
+    expect(httpResponse).toEqual(success({
+      status: 'SUCCESS',
+      user: {
+        id: 'any_id',
+        name: 'any_name',
+        email: 'any_email@mail.com',
+        password: 'any_password',
+        phoneNumber: '(00)1234-5678'
+      }
+    }))
+  })
+})
